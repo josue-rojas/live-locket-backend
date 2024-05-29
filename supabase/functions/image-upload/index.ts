6eacd9340444678c10388e89e55476077e8c4b96
josue <josuerojas.rojas@gmail.com>
2024-05-29 13:52:28 -0400

    [BUGFIX] Fix syntax error

--- supabase/functions/image-upload/index.ts
+++ supabase/functions/image-upload/index.ts
@@ -9,7 +9,7 @@ import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 // Create Supabase client
 const supabase = createClient(
   Deno.env.get('SUPABASE_URL') ?? Deno.env.get('LOCAL_SUPABASE_URL'),
-  Deno.env.get('SUPABASE_ANON_KEY')) ?? Deno.env.get('LOCAL_SUPABASE_ANON_KEY'));
+  Deno.env.get('SUPABASE_ANON_KEY') ?? Deno.env.get('LOCAL_SUPABASE_ANON_KEY'));
 
 // TODO:
 // - return proper http error code
