a3b853f9c456bd3cbf8e2b2a022228efdd4940c0
josue <josuerojas.rojas@gmail.com>
2024-05-28 22:52:08 -0400

    [FEAT] Create image upload function

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# live-locket-backend
-Backend stuff for the live locket
+# live-locket
+A project that uses a led matrix connected to a raspberry pi to display images that friends send you. 
+
+# Info
+This repo is for Backend stuff for the live locket
+
+There are three repos in total for this project. 
+- https://github.com/josue-rojas/live-locket
+- https://github.com/josue-rojas/live-locket-backend
+- https://github.com/josue-rojas/live-locket-pi
---
+++ supabase/functions/image-upload/index.ts
@@ -0,0 +1,57 @@
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
+
+// Setup type definitions for built-in Supabase Runtime APIs
+/// <reference types="https://esm.sh/@supabase/functions-js/src/edge-runtime.d.ts" />
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+
+// Create Supabase client
+const supabase = createClient(
+  Deno.env.get('LOCAL_SUPABASE_URL'),
+  Deno.env.get('LOCAL_SUPABASE_ANON_KEY'));
+
+// TODO:
+// - add default uploaded by
+// - return proper http error code
+// - createclient should look up if local env then it should use correct keys
+
+Deno.serve(async (req) => {
+  const response = {
+    success: false,
+  }
+
+  if (req.body) {
+    const body = await req.formData();
+    const imageFile = body.get("image");
+    const imagePath = `images/${imageFile.name}`;
+    
+    const { data, error } = await supabase.storage.from('images').upload(imagePath, imageFile);
+
+    if (error) {
+      response.error = error;
+    } else {
+      response.datas = data;
+      response.success = true;
+
+      const { error: dbError } = await supabase
+        .from('images')
+        .insert({
+          bucket_location: data.fullPath,
+          storage_id: data.id,
+          uploaded_by: "anon" 
+         });
+      
+      if (dbError) {
+        // TODO: if there is an error we should also delete the image or it will be not images not accounted for
+        response.error = dbError;
+      }
+    }
+  } else {
+    response.error = { message: "no body found" }
+  }
+
+  return new Response(JSON.stringify(response), 
+    { headers: { "Content-Type": "application/json" } },
+  )
+});
